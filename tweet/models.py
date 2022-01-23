a0f4a2f53f4a240a0dc12d4c20da4cf2ceefa3bf
geang <hsegeang@gmail.com>
2022-01-24 02:01:38 +0900

    2022.01.24-1

--- tweet/models.py
+++ tweet/models.py
@@ -1,3 +1,16 @@
+# tweet/models.py
 from django.db import models
+from user.models import UserModel # user폴더속에 models파일의 UserModel 이라는 속성을 가지고와서 사용한다.
+
 
 # Create your models here.
+class TweetModel(models.Model):
+    class Meta:
+        db_table = "tweet"
+    # 작성자
+    # ForeignKey - 다른데이터 베이스에 데이터를 가지고 오겠다라는 명령어
+    author = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+
+    content = models.CharField(max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- user/views.py
+++ user/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import UserModel
+
 
 # Create your views here.
+def sign_up_view(request):
+    if request.method == 'GET':
+        return render(request, 'user/signup.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        password2= request.POST.get('password2', None)
+        bio = request.POST.get('bio', None)
+
+        # password와 password2가 같이않다면
+        if password != password2:
+            return render (request,'user/signup.html')
+        else:
+            new_user = UserModel()
+            new_user.username = username
+            new_user.password = password
+            new_user.bio = bio
+        # 위의형태는 변수에 데이터를 담아놓은것이고 실제로는 데이터베이스에 데이터 정보를 저장되지 않는 상태이다.
+        # 밑에 save를 통해서 데이터베이스에 데이터를 넣어주는 역활을 한다.
+            new_user.save()
+        return redirect('/sign-in')
+
+
+def sign_in_view(request):
+    return render(request, 'user/signin.html')
